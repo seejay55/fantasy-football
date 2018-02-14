b3073bdae733b7b362aef6387d671ada9af9ca4f
Austin Berger <austin.berger2@gmail.com>
2018-02-14 13:08:01 -0600

    Added find-create-join page
    things and stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FindCreateJoinPageComponent } from './pages/find-create-join-page/find-create-join-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindCreateJoinPageComponent
   ],
   imports: [
     BrowserModule
