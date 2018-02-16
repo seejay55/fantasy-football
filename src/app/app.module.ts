a00f2f4662e49d361eeeb3cb6dc0013bfb59ce2a
thompsonl0075 <thompsonl0075@my.uwstout.edu>
2018-02-16 13:46:40 -0600

    loginbranch
    this be the login branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewsFeedComponent } from './components/homepage/news-feed/news-feed.component';
+import { FeedItemComponent } from './components/homepage/news-feed/feed-item/feed-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    NewsFeedComponent,
+    FeedItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
