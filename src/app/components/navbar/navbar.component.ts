98c8cda6d077e205aa81d405cd3c2a52568d4ebe
Nick Grey <nick5grey@gmail.com>
2018-02-28 12:29:45 -0600

    Initial persistent user sign in functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -45,7 +45,11 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   logIn(userName: string, password: string) {
     console.log({userName, password});
+<<<<<<< HEAD
     const requestingUser = new User(0, 'test@testuser.com' , userName, 'Nick', 'Grey');
+=======
+    const requestingUser = new User(0, userName, 'Nick', 'Grey');
+>>>>>>> Initial persistent user sign in functionality
     this.authService.login(requestingUser);
   }
 
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,13 +1,21 @@
 export class User {
   private _id;
+<<<<<<< HEAD
   private email;
+=======
+>>>>>>> Initial persistent user sign in functionality
   private userName;
   private firstName;
   private lastName;
 
+<<<<<<< HEAD
   constructor(_id: number, email: string, userName: string, firstName: string, lastName: string) {
     this._id = _id;
     this.email = email;
+=======
+  constructor(_id: number, userName: string, firstName: string, lastName: string) {
+    this._id = _id;
+>>>>>>> Initial persistent user sign in functionality
     this.userName = userName;
     this.firstName = firstName;
     this.lastName = lastName;
