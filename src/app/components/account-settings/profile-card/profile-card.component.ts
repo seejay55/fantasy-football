535348a16cec1c5060aca0d30301226b20400731
Nick Grey <nick5grey@gmail.com>
2018-02-28 12:29:45 -0600

    Initial persistent user sign in functionality

--- src/app/components/account-settings/profile-card/profile-card.component.ts
+++ src/app/components/account-settings/profile-card/profile-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from '../../../models/user';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.css']
 })
-export class ProfileCardComponent implements OnInit {
+export class ProfileCardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  user: User;
+  userSubscription: any;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.userSubscription = this.authService.getCurrentUser()
+      .subscribe(user => { this.user = user; });
   }
+
+  ngOnDestroy() { this.userSubscription.unsubscribe(); }
 
 }
