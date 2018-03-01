9085d42f7b4caab9cf734c02649172003e96a02d
Nick Grey <nick5grey@gmail.com>
2018-03-01 09:24:50 -0600

    added user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { BreadcrumbModule } from 'angular-crumbs';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     FormsModule,
     BreadcrumbModule,
     AppRoutingModule
