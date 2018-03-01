722985fb8ac37eec7b54ec06fba4678aa0736dcb
Nick Grey <nick5grey@gmail.com>
2018-03-01 09:24:50 -0600

    added user service

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,13 @@
 export class User {
   private _id;
+  private email;
   private userName;
   private firstName;
   private lastName;
 
-  constructor(_id: number, userName: string, firstName: string, lastName: string) {
+  constructor(_id: number, email: string, userName: string, firstName: string, lastName: string) {
     this._id = _id;
+    this.email = email;
     this.userName = userName;
     this.firstName = firstName;
     this.lastName = lastName;
