99d0884b763441cac6e18563630ef90867af7141
Nick Grey <nick5grey@gmail.com>
2018-03-07 15:21:16 -0600

    created functions for user.service and started league service (#23)

---
+++ src/app/services/league/league.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { League } from '../../models/league';
+
+@Injectable()
+export class LeagueService {
+
+  constructor() { }
+
+}
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -6,19 +6,41 @@ import { User } from '../../models/user';
 @Injectable()
 export class UserService {
 
-  endpoint = 'http://localhost:8000/api/user';
+  endpoint = 'http://localhost:8000/api';
 
   constructor(private http: HttpClient) { }
 
-  // public getAllUsers() { }
+  // /users
 
+  // return json of all users in database
+  public getAllUsers() {
+    return this.http.get(`${this.endpoint}/users`);
+  }
+
+  // return json of all users LIKE @userName
+  public getUserByUserName(userName: string) {
+    return this.http.get(`${this.endpoint}/users/${userName}`);
+  }
+
+  // send new user data, create that user, and return that user after database creation
+  public createUser(user: User, password: string) {
+    return this.http.post(`${this.endpoint}/users`, { body: { email: user.email, userName: user.userName, password: password } });
+  }
+
+
+  // /user
+
+  // send user id, get user pertaining to that id
   public getUser(id: number) {
     return this.http.get(`${this.endpoint}/user/${id}`);
   }
 
-  public createUser(user: User) { }
-  public updateUser(user: User) { }
+  // send updated user data, update that user, and return that user after database update
+  public updateUser(user: User) {
+    return this.http.patch(`${this.endpoint}/user/${user._id}`, { body: { userName: user.userName } });
+  }
 
+  // send id of user to be deleted, delete that user, then return status code.
   public deleteUser(id: number) {
     return this.http.delete(`${this.endpoint}/user/${id}`);
   }
