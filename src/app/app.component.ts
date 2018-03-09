a33a0d84fc173c888e5157a2c7b6dca28c98e85a
Nick Grey <nick5grey@gmail.com>
2018-03-09 11:17:07 -0600

    added alert service for application-wide alerts for everyone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
 import { UserService } from './services/user/user.service';
 import { BreadcrumbService } from 'angular-crumbs';
+import { AlertService } from './shared/services/alert.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AuthService, BreadcrumbService]
+  providers: [
+    AuthService,
+    BreadcrumbService,
+    AlertService
+  ]
 })
 export class AppComponent {
   title = 'Fantasy Football Website';
