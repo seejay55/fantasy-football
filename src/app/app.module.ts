97f2f6e08323fac90a2223956003e53218c4a429
thompsonl0075 <thompsonl0075@my.uwstout.edu>
2018-03-23 13:28:50 -0500

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
-=======
->>>>>>> origin/luke_scoringleaders
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app.routing';
 
@@ -14,29 +11,17 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { BreadcrumbModule } from 'angular-crumbs';
-=======
-import { BreadcrumbModule } from 'angular-crumbs';
-import { ScoringLeadersPageComponent } from './pages/scoring-leaders-page/scoring-leaders-page.component';
->>>>>>> origin/luke_scoringleaders
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-<<<<<<< HEAD
-=======
-    ScoringLeadersPageComponent,
->>>>>>> origin/luke_scoringleaders
   ],
   imports: [
     BrowserModule,
     HttpModule,
-<<<<<<< HEAD
     HttpClientModule,
-=======
->>>>>>> origin/luke_scoringleaders
     FormsModule,
     BreadcrumbModule,
     AppRoutingModule
@@ -44,8 +29,4 @@ import { ScoringLeadersPageComponent } from './pages/scoring-leaders-page/scorin
   providers: [],
   bootstrap: [AppComponent]
 })
-<<<<<<< HEAD
 export class AppModule { }
-=======
-export class AppModule { }
->>>>>>> origin/luke_scoringleaders
