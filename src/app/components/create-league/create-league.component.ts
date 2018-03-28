f6f0b65b7466c31bc014c4ec28ce6a537689efad
Nick Grey <nick5grey@gmail.com>
2018-03-28 15:17:25 -0500

    started league find and crate pages

--- src/app/components/create-league/create-league.component.ts
+++ src/app/components/create-league/create-league.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { League } from '../../models/league';
+
+import { LeagueService } from '../../services/league/league.service';
+import { AlertService } from '../../shared/services/alert.service';
+
 @Component({
   selector: 'app-create-league',
   templateUrl: './create-league.component.html',
-  styleUrls: ['./create-league.component.css']
+  styleUrls: ['./create-league.component.css'],
+  providers: [LeagueService]
 })
 export class CreateLeagueComponent implements OnInit {
 
-  constructor() { }
+  constructor(private leagueService: LeagueService, private alertService: AlertService) { }
 
-  ngOnInit() { }
+  ngOnInit() {  }
 
-  send() {
+  createLeague(name: string, userId: number, priv: boolean, pub: boolean, unlimited: boolean, other: number, maxTeams: number): boolean {
+    // after computation
+    name = 'NickLeague2';
+    userId = 10;
+    const privacy = 'Public';
+    const maxTrades = 1;
+    maxTeams = 11;
+    /* this.leagueService.createLeague(name, userId, privacy, maxTrades, maxTeams).subscribe(
+      success => this.alertService.success('Success', `League ${name} been created`, false),
+      err => console.log(err)
+    ); */
+    return false;
   }
 
 }
