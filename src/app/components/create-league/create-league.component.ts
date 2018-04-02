1b0d773522e4fd7f765487f3d1a102fc6326d14b
Nick Grey <nick5grey@gmail.com>
2018-04-02 12:19:25 -0500

    Find league (#42)
    * started league find and crate pages
    
    * Hopefully this works- Connection refused when testing

--- src/app/components/create-league/create-league.component.ts
+++ src/app/components/create-league/create-league.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { League } from '../../models/league';
+
+import { LeagueService } from '../../services/league/league.service';
+import { AlertService } from '../../shared/services/alert.service';
+
 @Component({
   selector: 'app-create-league',
   templateUrl: './create-league.component.html',
-  styleUrls: ['./create-league.component.css']
+  styleUrls: ['./create-league.component.css'],
+  providers: [LeagueService]
 })
 export class CreateLeagueComponent implements OnInit {
 
-  constructor() { }
+  constructor(private leagueService: LeagueService, private alertService: AlertService) { }
 
-  ngOnInit() { }
+  ngOnInit() {  }
 
-  send() {
+  createLeague(name: string, userId: number, privacy: string, unlimited: string, other: string, maxTeams: number): boolean {
+    let maxTrades;
+
+    if(unlimited === "Unlimited")
+    {
+        const maxTrades = Number.MAX_SAFE_INTEGER;
+    } else
+    {
+        const maxTrades = other;
+    }
+
+    this.leagueService.createLeague(name, userId, privacy, maxTrades, maxTeams).subscribe(
+      success => this.alertService.success('Success', `League ${name} been created`, false),
+      err => console.log(err)
+    );
+    return false;
   }
 
 }
